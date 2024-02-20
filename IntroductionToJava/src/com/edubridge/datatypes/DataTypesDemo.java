f2896f9783d129860b720a8bed347f408b580136
jerry-45 <vishwa.atul16@gmail.com>
2024-02-20 14:54:54 +0530

    commit

---
+++ IntroductionToJava/src/com/edubridge/datatypes/DataTypesDemo.java
@@ -0,0 +1,10 @@
+package com.edubridge.datatypes;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IntroductionToJava/src/com/edubridge/simplejavaprogram/HelloWorldDemo.java
@@ -0,0 +1,10 @@
+package com.edubridge.simplejavaprogram;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+
+}
