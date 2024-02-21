b89572e40dd6f6de7d9ccc2411e55fe65bdd37ff
jerry-45 <vishwa.atul16@gmail.com>
2024-02-21 16:01:03 +0530

    commit

---
+++ IntroductionToJava/src/com/edubridge/datatypes/Bitwise.java
@@ -0,0 +1,18 @@
+package com.edubridge.datatypes;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5&7);
+		System.out.println(5|7);
+		System.out.println(5^7);
+		System.out.println(5>>7);
+		System.out.println(5<<7);
+
+
+
+
+	}
+
+}
