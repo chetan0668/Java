248953cc6d1b09d3b97d3e4f95ef77458e623fbe
jerry-45 <vishwa.atul16@gmail.com>
2024-02-21 15:41:21 +0530

    commit

---
+++ IntroductionToJava/src/com/edubridge/datatypes/Ternary.java
@@ -0,0 +1,12 @@
+package com.edubridge.datatypes;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		int num = 9;
+		String res = (num%2==1)? "Even":"Odd";
+		System.out.println(res);
+
+	}
+
+}
